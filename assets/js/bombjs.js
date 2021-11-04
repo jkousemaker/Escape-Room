8e096d748f62cca59e25623b726aeb56e681ce29
Justin Kousemaker <j.l.kousemaker@Gmail.com>
2021-11-04 12:59:25 +0100

    huh

--- assets/js/bombjs.js
+++ assets/js/bombjs.js
@@ -1,21 +1,22 @@
-/* To do:
-    1. Improve styling.
-    2. Photoshop buttons of original picture.
-*/
+/* Variables */
 
+//Stores all necessary elements in variables.
 const buttons = document.querySelectorAll(".button");
 const newButton = document.querySelector(".restart-button");
 const bombTimer = document.querySelector(".bomb-timer");
 const bombCombination = document.querySelector(".bomb-combination");
 const gameResult = document.querySelector(".result");
 const timerReduce = document.querySelector(".reduce");
+const nextButton = document.querySelector(".next-game");
 
+//Stores all audio files in variables.
 const plantAudio = document.querySelector(".planted");
 const inputAudio = document.querySelector(".input");
 const defusedAudio = document.querySelector(".defused");
 const explosionAudio = document.querySelector(".explosion");
 const ticksAudio = document.querySelector(".ticks");
 
+//Necessary variables for the game.
 let interval;
 let interval1;
 let counter;
@@ -25,16 +26,18 @@ let result;
 let lastBut;
 let sec;
 
-/*addEventListeners*/
+/*AddEventListeners*/
 
 //Starts a new game.
 newButton.addEventListener('click', StartNewGame);
 
-
+//Makes sure the buttons work when pressed.
 for (let i = 0; i < buttons.length; i++){
     buttons[i].addEventListener('click', (e) => {
+        //Checks if a game has been started
         if (game) {
             inputAudio.play();
+            //Switch case statement that checks how many numbers have been given and if the chosen number is correct.
             switch(counter) {
                 case 0: if(buttons[i].textContent == numbers[0]) {
                     Correct(buttons[i].textContent);
@@ -72,20 +75,26 @@ for (let i = 0; i < buttons.length; i++){
     })
 }
 
-/*functions*/
+/*Functions*/
 
 //Start a new game, resetting all variables.
 function StartNewGame() {
+    //Sets up all variable for a new game.
     counter = 0;
     numbers = Randomize();
-    console.log("Unique randomized numbers : " + numbers);
     game = true;
+    bombCombination.textContent = "";
+    console.clear();
+
+    console.log("Unique randomized numbers : " + numbers);
+
+    //Gets rid of the winner/loser indicator if played has played match already.
     if (result) {
         gameResult.classList.remove("winner");
     } else if (!result) {
         gameResult.classList.remove("loser");
     }
-    bombCombination.textContent = "";
+
     TimerUpdate();
     plantAudio.play();
 }
@@ -93,10 +102,13 @@ function StartNewGame() {
 //Chooses 4 random and unique numbers(int) to use as the answer to the combination of the bomb.
 function Randomize() {
     let numbers = [Math.floor(Math.random() * 10)];
+
+    //While loop that keeps looping until the array numbers[] has 4 unique numbers.
     while(numbers.length < 4){
         let r = Math.floor(Math.random() * 10);
         let count = 0
 
+        //Loops through all the numbers of the array to make sure the new number isn't a duplicate.
         for (let i = 0; i < numbers.length; i++) {
             if(r != numbers[i]) {
                 count++;
@@ -110,56 +122,68 @@ function Randomize() {
     return numbers;
 }
 
+//Updates the timer if new game has been started.
 function TimerUpdate() {
-    sec = 40;
+    sec = 30;
+
+    //setInterval function that keeps on looping until there's either no time left or the player wins.
     interval = setInterval(function() {
         sec--;
-        bombTimer.textContent = sec + "s";
-        console.log(sec);
+
+        if (sec <= 0) {
+            bombTimer.textContent = 0 + "s";
+        } else {
+            bombTimer.textContent = sec + "s";
+        }
+
         if (sec <= 24) {
             ticksAudio.play();
         }
+
         if (sec <= 0) {
-            console.log("Time over.");
             Lose();
         }
     }, 1000);
 }
 
+//Function for if the player put in the correct answer.
 function Correct(input) {
     counter++
-    console.log("correct");
+
     if (lastBut) {
         lastBut.classList.remove("button-incorrect");
     }
+
     bombCombination.textContent += input;
 }
 
 //Resets the correct answer counter and reduces the total time left.
 function False(but) {
-    console.log("false");
     buttons[but].classList.add("button-incorrect");
+
     if (lastBut) {
         lastBut.classList.remove("button-incorrect");
     }
+
     lastBut = buttons[but];
     bombCombination.textContent = "";
 
+    //Switch case statement that reduces the total time left depending on how many correct answers the player has given already.
     switch (counter) {
         case 0: 
-            reduce = 10;
+            reduce = 5;
             ReduceTimer(reduce);
             break;
         case 1:
-            reduce = 15;
+            reduce = 7;
             ReduceTimer(reduce);
             break;
         case 2:
-            reduce = 25;
+            reduce = 12;
             ReduceTimer(reduce);
             break;
         case 3:
-            reduce = 30;
+            reduce = 20;
             ReduceTimer(reduce);
             break;
     }
@@ -167,15 +191,19 @@ function False(but) {
     counter = 0;
 }
 
+//Updates the timer if wrong number has been answered.
 function ReduceTimer(time) {
     let toggle = 0;
     sec -= reduce;
+
     timerReduce.textContent = "-" + time;
     timerReduce.classList.add("visible")
+
+    //setInterval function it's core purpose is to make the time penalty indicator disappear.
     interval1 = setInterval(function() {
         timerReduce.classList.remove("visible");
         toggle++;
-        console.log(toggle);
+
         if(toggle >= 1) {
             clearInterval(interval1);
         }
@@ -185,12 +213,17 @@ function ReduceTimer(time) {
 //Runs if player has won.
 function Win() {
     clearInterval(interval);
-    console.log("you've won");
+
     ticksAudio.pause();
     ticksAudio.currentTime = 0;
+
     defusedAudio.play();
+
     gameResult.classList.add("winner");
     gameResult.textContent = "Winner!";
+
+    nextButton.classList.add("visible");
+
     game = false;
     result = true;
 }
@@ -200,12 +233,13 @@ function Lose() {
     clearInterval(interval);
 
     explosionAudio.play();
+
     ticksAudio.pause();
     ticksAudio.currentTime = 0;
 
-    console.log("you've lost");
     gameResult.textContent = "Loser!";
     gameResult.classList.add("loser");
+
     game = false;
     result = false;
 }
